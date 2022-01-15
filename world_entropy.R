c4935654444a1317b67fc58fb1c501a9da2868b9
Ryan McCorvie <ryan@mccorvie.org>
2022-01-15 12:11:27 -0800

    remove duplicated functions, tune up first word checker

--- world_entropy.R
+++ world_entropy.R
@@ -2,10 +2,13 @@ library( tidyverse)
 
 intersect(readLines( "wordle_solution.csv"),readLines( "wordle_valid.csv"))
 
-wordle_solution <- str_split(sort(readLines( "wordle_solution.csv")), "" )
-wordle_valid    <- str_split(sort(readLines( "wordle_valid.csv")), "")
-
 wordle_solution_words <- sort(readLines( "wordle_solution.csv"))
+wordle_valid_words <- sort(readLines( "wordle_valid.csv"))
+
+wordle_solution <- str_split(wordle_solution_words, "" )
+wordle_valid    <- str_split(wordle_valid_words, "")
+
+
 includes_let <- map( letters, ~ str_subset( wordle_solution_words, .)) 
 excludes_let <- map( letters, ~ str_subset( wordle_solution_words, ., negate=T) )
 names( includes_let ) <- names( excludes_let ) <- letters
@@ -77,44 +80,5 @@ game_state_from_feedback <- function( guess_log, feedback_log, game_state = init
   game_state
 }
 
-wordle_solution <- str_split(sort(readLines( "wordle_solution.csv")), "" )
-wordle_valid    <- str_split(sort(readLines( "wordle_valid.csv")), "")
 
-wordle_solution_words <- sort(readLines( "wordle_solution.csv"))
-includes_let <- map( letters, ~ str_subset( wordle_solution_words, .)) 
-excludes_let <- map( letters, ~ str_subset( wordle_solution_words, ., negate=T) )
-names( includes_let ) <- names( excludes_let ) <- letters
-
-
-wordle_freq <- sort(table(unlist(map( str_split( wordle_valid, ""), unique))), decreasing=T)
-#wordle_valid_full <- keep( str_split( wordle_valid, ""), ~ length( unique(.))==5) 
-#keep( wordle_valid_full, ~ all( . %in% names(wordle_freq)[1:5]))
-
-update_state <- function( game_state, guess, solution )
-{
-  game_state$in_solution <- union( game_state$in_solution, guess[guess %in% solution] )
-  game_state$black <- union( game_state$black, guess[ !( guess %in% solution )])
-  game_state$green[guess==solution] = guess[guess==solution]
-  game_state$yellow <- if_else( guess %in% solution & guess != solution, paste0( game_state$yellow, guess), game_state$yellow )
-  game_state
-}
-
-filter_solutions_slow <- function( possible_solutions, game_state )
-{
-  possible_solutions <- keep( possible_solutions, ~ all( game_state$green == "" | . == game_state$green ))
-  possible_solutions <- keep( possible_solutions, ~ all( game_state$in_solution %in% . ))
-  possible_solutions <- keep( possible_solutions, ~ !any( . %in% game_state$black ))
-  possible_solutions <- keep( possible_solutions, ~ !any( str_detect( game_state$yellow, . )))
-  possible_solutions
-}
-
-filter_solutions1st <- function( guess, solution )
-{
-  possible_solutions <- str_split( reduce( c( includes_let[ intersect( guess, solution )], excludes_let[ setdiff( guess, solution )] ), intersect ), "")
-  green <- guess==solution
-  possible_solutions <- keep( possible_solutions, ~ all( .[green] == solution[green] ))
-  yellow <- guess %in% solution & guess != solution
-  possible_solutions <- keep( possible_solutions, ~ all( .[yellow] != guess[yellow] ))
-  possible_solutions
-}
 
